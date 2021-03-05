7d4227dea886e636a43035e3f5fea967b7105ccc
John Neville <johnpneville@gmail.com>
2021-03-04 21:19:18 -0500

    Initial commit for a basic action that will automatically manage org action runners

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
-# self-hosted-runner-grouper
+# Org Self Hosted Runners Grouper
+
+Org Self Hosted Runners grouper triages PRs based on the paths that are modified in the PR.
+
+## Usage
+
+### Create `.github/action-grouper.yml`
+
+Create a `.github/action-grouper.yml` file with a list of groups and [minimatch](https://github.com/isaacs/minimatch) globs to match to apply the group.
+
+The key is the name of the group in your repository that you want to add (eg: "merge conflict", "needs-updating") and the value is the path (glob) of the changed files (eg: `src/**/*`, `tests/*.spec.js`) or a match object.
+
+#### Match Object
+
+For more control over matching, you can provide a match object instead of a simple path glob. The match object is defined as:
+
+```yml
+- any: ['list', 'of', 'globs']
+  all: ['list', 'of', 'globs']
+```
+
+One or both fields can be provided for fine-grained matching. Unlike the top-level list, the list of path globs provided to `any` and `all` must ALL match against a path for the group to be applied.
+
+The fields are defined as follows:
+* `any`: match ALL globs against ANY changed path
+* `all`: match ALL globs against ALL changed paths
+
+A simple path glob is the equivalent to `any: ['glob']`. More specifically, the following two configurations are equivalent:
+```yml
+group1:
+- example1/*
+```
+and
+```yml
+group1:
+- any: ['example1/*']
+```
+
+From a boolean logic perspective, top-level match objects are `OR`-ed together and indvidual match rules within an object are `AND`-ed. Combined with `!` negation, you can write complex matching rules.
+
+#### Basic Examples
+
+```yml
+# Add 'group1' to any changes within 'example' folder or any subfolders
+group1:
+  - example/**/*
+
+# Add 'group2' to any file changes within 'example2' folder
+group2: example2/*
+```
+
+#### Common Examples
+
+```yml
+# Add 'repo' group to any root file changes
+repo:
+  - ./*
+  
+# Add '@domain/core' group to any change within the 'core' package
+@domain/core:
+  - package/core/*
+  - package/core/**/*
+
+# Add 'test' group to any change to *.spec.js files within the source dir
+test:
+  - src/**/*.spec.js
+
+# Add 'source' group to any change to src files within the source dir EXCEPT for the docs sub-folder
+source:
+- any: ['src/**/*', '!src/docs/*']
+
+# Add 'frontend` group to any change to *.js files as long as the `main.js` hasn't changed
+frontend:
+- any: ['src/**/*.js']
+  all: ['!src/main.js']
+```
+
+### Create Workflow
+
+Create a workflow (eg: `.github/workflows/action-grouper.yml` see [Creating a Workflow file](https://help.github.com/en/articles/configuring-a-workflow#creating-a-workflow-file)) to utilize the grouper action with content:
+
+```
+name: "Org Self Hosted Runners Grouper"
+on:
+- pull_request_target
+
+jobs:
+  triage:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/grouper@main
+      with:
+        repo-token: "${{ secrets.GITHUB_TOKEN }}"
+```
+
+_Note: This grants access to the `GITHUB_TOKEN` so the action can make calls to GitHub's rest API_
+
+#### Inputs
+
+Various inputs are defined in [`action.yml`](action.yml) to let you configure the grouper:
+
+| Name | Description | Default |
+| - | - | - |
+| `repo-token` | Token to use to authorize group changes. Typically the GITHUB_TOKEN secret | N/A |
+| `configuration-path` | The path to the group configuration file | `.github/action-grouper.yml` |
+| `sync-groups` | Whether or not to remove groups when matching files are reverted or no longer changed by the PR | `false`
---
+++ src/main.ts
@@ -0,0 +1,308 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Octokit } from "@octokit/rest";
+import { GetResponseDataTypeFromEndpointMethod} from "@octokit/types";
+import * as yaml from "js-yaml";
+import { IMinimatch, Minimatch } from "minimatch";
+import { ListReposWithAccessToSelfHostedRunnerGroupInOrgResponse, ListSelfHostedRunnersGroupResponse, RepoTypes, RunnerGroup, StringOrMatchConfig, MatchConfig } from "./types";
+
+async function run() {
+  try {
+    const token = core.getInput("org-auth-token", { required: true });
+    const orgName = core.getInput("org-name", { required: true });
+    const configPath = core.getInput("configuration-path", { required: true });
+    const repoType = core.getInput("org-repo-type", { required: true });
+    const shouldOverwrite = core.getInput("should-overwrite", { required: false });
+    const shouldCreateMissingGroups = core.getInput("should-create-missing", { required: false });
+
+    const client = new github.GitHub(token);
+
+    // Load up all runners for the github org
+    const listForOrgOptions = client.repos.listForOrg.endpoint.merge({
+      org: orgName,
+      type: repoType as RepoTypes
+    });
+  
+    type listRepoResponseType = GetResponseDataTypeFromEndpointMethod<typeof client.repos.listForOrg>;
+    const repositories = (await client.paginate(listForOrgOptions)) as listRepoResponseType;
+
+    // Get the existing runner groups
+    const existingRunnerGroups = await getExistingRunnerGroups(
+      client,
+      orgName
+    )
+
+    const groupGlobs: Map<string, StringOrMatchConfig[]> = await getGroupGlobs(
+      client,
+      configPath
+    );
+
+    // Validate managed runner groups
+    const groupsThatAreValid: Map<RunnerGroup, StringOrMatchConfig[]> = new Map();
+    const groupsToAdd:  Map<string, StringOrMatchConfig[]> = new Map();
+    const invalidGroups: string[] = [];
+    for (const [group, globs] of groupGlobs.entries()) {
+      core.debug(`validating ${group}`);
+      const matchingExistingGroup = existingRunnerGroups.filter(g => g.name == group)[0]
+      if(!matchingExistingGroup){
+        groupsToAdd.set(group, globs);
+      }
+      else if(isSupportedRunnerGroup(matchingExistingGroup)){
+        groupsThatAreValid.set(matchingExistingGroup, globs);
+      }
+      else{
+        invalidGroups.push(group);
+      }
+    }
+
+    // Sync existing managed runner groups with repos
+    for (const [existingGroup, globs] of groupsThatAreValid.entries()) {
+      core.debug(`syncing ${existingGroup.name}`);
+      await syncExistingGroupToRepo(client, orgName, existingGroup.id, repositories, globs, shouldOverwrite)
+    }
+
+    // Create missing managed runner groups with matching repos
+    if(shouldCreateMissingGroups){
+      for (const [group, globs] of groupsToAdd.entries()) {
+        core.debug(`creating ${group}`);
+        await addMissingGroupToRepo(client, orgName, group, repositories, globs)
+      }
+    }
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+function isSupportedRunnerGroup(group : RunnerGroup): boolean {
+  core.debug(`    checking to see if ${group.name} is supported`);
+  if(group.visibility != "selected"){
+    core.warning(`the group(${group.name}) must be marked as "selected" visibility for it to be supported`);
+  }
+  core.debug(`    group is supported`);
+  return true;
+}
+
+async function getGroupGlobs(
+  client: github.GitHub,
+  configurationPath: string
+): Promise<Map<string, StringOrMatchConfig[]>> {
+  const configurationContent: string = await fetchContent(
+    client,
+    configurationPath
+  );
+
+  // loads (hopefully) a `{[group:string]: string | StringOrMatchConfig[]}`, but is `any`:
+  const configObject: any = yaml.safeLoad(configurationContent);
+
+  // transform `any` => `Map<string,StringOrMatchConfig[]>` or throw if yaml is malformed:
+  return getGroupGlobMapFromObject(configObject);
+}
+
+async function fetchContent(
+  client: github.GitHub,
+  repoPath: string
+): Promise<string> {
+  const response: any = await client.repos.getContents({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    path: repoPath,
+    ref: github.context.sha
+  });
+
+  return Buffer.from(response.data.content, response.data.encoding).toString();
+}
+
+function getGroupGlobMapFromObject(
+  configObject: any
+): Map<string, StringOrMatchConfig[]> {
+  const groupGlobs: Map<string, StringOrMatchConfig[]> = new Map();
+  for (const group in configObject) {
+    if (typeof configObject[group] === "string") {
+      groupGlobs.set(group, [configObject[group]]);
+    } else if (configObject[group] instanceof Array) {
+      groupGlobs.set(group, configObject[group]);
+    } else {
+      throw Error(
+        `found unexpected type for group ${group} (should be string or array of globs)`
+      );
+    }
+  }
+
+  return groupGlobs;
+}
+
+function printPattern(matcher: IMinimatch): string {
+  return (matcher.negate ? "!" : "") + matcher.pattern;
+}
+
+function toMatchConfig(config: StringOrMatchConfig): MatchConfig {
+  if (typeof config === "string") {
+    return {
+      any: [config]
+    };
+  }
+
+  return config;
+}
+
+function checkGlobs(
+  repoName: string,
+  globs: StringOrMatchConfig[]
+): boolean {
+  for (const glob of globs) {
+    core.debug(` checking pattern ${JSON.stringify(glob)}`);
+    const matchConfig = toMatchConfig(glob);
+    if (checkMatch(repoName, matchConfig)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkMatch(repoName: string, matchConfig: MatchConfig): boolean {
+  if (matchConfig.all !== undefined) {
+    if (!checkAll(repoName, matchConfig.all)) {
+      return false;
+    }
+  }
+
+  if (matchConfig.any !== undefined) {
+    if (!checkAny(repoName, matchConfig.any)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+// equivalent to "Array.some()" but expanded for debugging and clarity
+function checkAny(repoName: string, globs: string[]): boolean {
+  const matchers = globs.map(g => new Minimatch(g));
+  core.debug(`  checking "any" patterns`);
+  if (isMatch(repoName, matchers)) {
+    core.debug(`  "any" patterns matched against ${repoName}`);
+    return true;
+  }
+
+  core.debug(`  "any" patterns did not match any files`);
+  return false;
+}
+
+// equivalent to "Array.every()" but expanded for debugging and clarity
+function checkAll(repoName: string, globs: string[]): boolean {
+  const matchers = globs.map(g => new Minimatch(g));
+  core.debug(` checking "all" patterns`);
+  if (!isMatch(repoName, matchers)) {
+    core.debug(`  "all" patterns did not match against ${repoName}`);
+    return false;
+  }
+  core.debug(`  "all" patterns matched all files`);
+  return true;
+}
+
+function isMatch(repoName: string, matchers: IMinimatch[]): boolean{
+  core.debug(`    matching patterns against repoName ${repoName}`);
+  for (const matcher of matchers) {
+    core.debug(`   - ${printPattern(matcher)}`);
+    if (!matcher.match(repoName)) {
+      core.debug(`   ${printPattern(matcher)} did not match`);
+      return false;
+    }
+  }
+
+  core.debug(`   all patterns matched`);
+  return true;
+}
+
+function getMatchingReposIds(
+  repositories: Octokit.ReposListForOrgResponse, 
+  globs: StringOrMatchConfig[]
+) : number[] {
+  const repositoryIds: number[] = []
+  for(const [n,repo] of repositories.entries()){
+    if(checkGlobs(repo.name, globs)){
+      repositoryIds.push(repo.id)
+    }
+  }
+  return repositoryIds
+}
+
+async function getExistingRunnerGroups(
+  client: github.GitHub,
+  orgName: string
+): Promise<Array<RunnerGroup>> {
+  const apiResponse = await client.request('GET /orgs/{org}/actions/runner-groups', {
+    org: orgName
+  })
+  const orgRunnerGroupsResponse = apiResponse.data as ListSelfHostedRunnersGroupResponse
+  return orgRunnerGroupsResponse.runner_groups
+}
+
+async function syncExistingGroupToRepo(
+  client: github.GitHub,
+  orgName: string,
+  runnerGroupId: number,
+  repositories: Octokit.ReposListForOrgResponse, 
+  globs: StringOrMatchConfig[],
+  shouldOverwrite : string
+){
+  const repositoryIds: number[] = []
+  if(!shouldOverwrite){
+    const existingRepos = await getSelectedReposForRunnerGroups(client, orgName, runnerGroupId)
+    if(existingRepos.repositories){
+      for(const [i,repo] of existingRepos.repositories?.entries() ){
+        repositoryIds.push(repo.id)
+      }
+    }
+  }
+
+  const matchingRepoIds = getMatchingReposIds(repositories, globs)
+  const allRepositoryIds = repositoryIds.concat(matchingRepoIds)
+
+  setSelectedReposForRunnerGroups(client, orgName, runnerGroupId, allRepositoryIds)
+}
+
+async function addMissingGroupToRepo(
+  client: github.GitHub,
+  orgName: string,
+  groupName: string,
+  repositories: Octokit.ReposListForOrgResponse, 
+  globs: StringOrMatchConfig[]
+){
+  const matchingRepoIds = getMatchingReposIds(repositories, globs)
+  await client.request('POST /orgs/{org}/actions/runner-groups', {
+    org: orgName,
+    name: groupName,
+    selected_repository_ids: matchingRepoIds,
+    visibility: "selected"
+  })
+}
+
+async function getSelectedReposForRunnerGroups(
+  client: github.GitHub,
+  orgName: string,
+  runnerGroupId: number,
+): Promise<ListReposWithAccessToSelfHostedRunnerGroupInOrgResponse> {
+  const apiResponse = await client.request('GET /orgs/{org}/actions/runner-groups/{runner_group_id}/repositories', {
+    org: orgName,
+    runner_group_id: runnerGroupId
+  })
+  return (apiResponse.data as ListReposWithAccessToSelfHostedRunnerGroupInOrgResponse)
+}
+
+async function setSelectedReposForRunnerGroups(
+  client: github.GitHub,
+  orgName: string,
+  runnerGroupId: number,
+  repositoryIds: number[]
+) {
+  await client.request('PUT /orgs/{org}/actions/runner-groups/{runner_group_id}/repositories', {
+    org: orgName,
+    runner_group_id: runnerGroupId,
+    selected_repository_ids: repositoryIds
+  })
+}
+
+run();
